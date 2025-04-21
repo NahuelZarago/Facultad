38dd604fe719eedb40fccfdfb81a15da132720f8
Nahuel <nahuelzarax@gmail.com>
2025-04-21 17:43:03 -0300

    modifique el ejercicio 3

--- eje5_tp1.py
+++ eje5_tp1.py
@@ -1,26 +1,23 @@
 def romano(n):
-  letras= {
-    'I': 1,
-    'V': 5,
-    'X': 10,
-    'L': 50,
-    'C': 100,
-    'D': 500,
-    'M': 1000
-  }
-  
-  end = 0
-  before = 0
-  
-  for i in n:
-    letritas = letras[i]
-    if before and letritas > before:
-        end += letritas - 2 * before
-    else:
-        end += letritas 
-    before =  letritas
-  
-  return end
+    letras = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    }
 
-n = input('Ingrese un numero romano (mayusculas): ')
-print ('Decimal: ', romano(n))
+    if not n:
+        return 0
+    if len(n) > 1 and letras[n[0]] < letras[n[1]]:
+        return -letras[n[0]] + romano(n[1:])
+    return letras[n[0]] + romano(n[1:])
+
+
+print(romano('V'))     
+print(romano('IV'))    
+print(romano('IX'))    
+print(romano('XIV'))   
+
